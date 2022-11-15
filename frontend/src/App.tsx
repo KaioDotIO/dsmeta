9d4c6f3e9589ddd2235bada13bd41e4410b7d2f3
k_ribeiro <kaio.ribeiro@bravi.com.br>
2022-11-15 13:23:44 -0300

    components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import Header from "./components/Header";
 import NotificationButton from "./components/NotificationButton";
+import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
     <>
-      <div>asd</div>
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   );
 }
