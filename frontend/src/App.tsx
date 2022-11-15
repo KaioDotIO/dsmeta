f6ab3303b271cb96ee95e14dd376e3330195abe7
k_ribeiro <kaio.ribeiro@bravi.com.br>
2022-11-15 12:53:24 -0300

    first component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
+import NotificationButton from "./components/NotificationButton";
+
 function App() {
   return (
-    <h1>
+    <>
       <div>asd</div>
-    </h1>
+      <NotificationButton />
+    </>
   );
 }
 
